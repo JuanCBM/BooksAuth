2b34ce1297ce93a611940fd4e95e8f33eefc8585
mahuerta <miguel5692@gmail.com>
2021-01-17 18:37:54 +0100

    updateCode and clean.

--- Node/src/routes/bookRouter.js
+++ Node/src/routes/bookRouter.js
@@ -5,6 +5,7 @@ const User = require('../models/user.js').User;
 const toResponseComment = require('../models/comment.js').toResponse;
 const mongoose = require('mongoose');
 const VerifyToken = require('../services/verifyToken');
+const {ObjectId} = require("bson");
 
 const INVALID_BOOK_ID_RESPONSE = { "error": "Invalid book id" };
 const BOOK_NOT_FOUND_RESPONSE = { "error": "Book not found" }
@@ -119,16 +120,16 @@ router.delete('/:id/comments/:commentId', VerifyToken, async (req, res) => {
 });
 
 router.delete('/:id', VerifyToken, async (req,res)=> {
-    const bookId = req.params.idBook;
+    const bookId = req.params.id;
 
     if (!mongoose.Types.ObjectId.isValid(bookId)) {
         return res.status(400).send(INVALID_BOOK_ID_RESPONSE);
     }
-    const book = await Book.findById(id);
+    const book = await Book.findById(bookId).populate('comments.user');
     if (!book) {
         return res.status(404).send(BOOK_NOT_FOUND_RESPONSE);
     }
-    book.remove()
+    await Book.findByIdAndDelete(bookId);
     res.json(toResponseBook(book));
 })
 
@@ -138,32 +139,20 @@ router.put('/:id', VerifyToken, async (req,res)=> {
     if(!mongoose.Types.ObjectId.isValid(bookId)){
         return res.sendStatus(400);
     }
-    const persistedBook = await Book.findById(bookId);
+    const book = await Book.findById(bookId).populate('comments.user');
 
-    const book = new Book({
-        title: req.body.title,
-        resume: req.body.resume,
-        author: req.body.author,
-        editorial: req.body.editorial,
-        publicationYear: req.body.publicationYear,
-    });
-
-    if (!persistedBook) {
-        res.sendStatus(404);
-    } else {
-        persistedBook.title = book.title;
-        persistedBook.author = book.author;
-        persistedBook.resume = book.resume;
-        persistedBook.editorial = book.editorial;
-        persistedBook.publicationYear = book.publicationYear;
+    if (!book) {
+        return res.status(404).send(BOOK_NOT_FOUND_RESPONSE);
     }
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.editorial = req.body.editorial;
+    book.resume = req.body.resume;
+    book.publicationYear = req.body.publicationYear;
 
-    await persistedBook.save()
-    .then(savedBook => res.json(toResponseBook(savedBook)))
-    .catch(error => {
-        console.log(error);
-        res.status(400).send(error);
-    });
+    const updatedBook =await book.save();
+
+    res.json(toResponseBook(updatedBook));
 })
 
 module.exports = router;
