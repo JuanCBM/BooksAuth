f174455c00e6a9fadaf3a91683c4e641a8bcf0d2
mahuerta <miguel5692@gmail.com>
2021-01-17 14:09:20 +0100

    Modificacion script

--- exec_dockers.js
+++ exec_dockers.js
@@ -10,5 +10,9 @@ function exec(serviceName, command){
 }
 
 
+exec('MongoDBStop', 'docker stop mongo-db');
+exec('MySQLStop', 'docker stop mysql-db');
+exec('MongoDBRemove', 'docker rm mongo-db');
+exec('MySQLRemove', 'docker rm mysql-db');
 exec('MongoDB', 'docker run -d -p 27017:27017 --name mongo-db mongo:latest');
 exec('MySQL', 'docker run -p 3306:3306 --name mysql-db -e MYSQL_ROOT_PASSWORD=pass -e MYSQL_DATABASE=test -e -d mysql:latest');
